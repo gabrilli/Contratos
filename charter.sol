57f72bb6b55238c5fdfd9c2daa5f08b5836b2d73
gabrilli <50384585+gabrilli@users.noreply.github.com>
2019-05-20 16:18:17 -0300

    Create charter.sol

---
+++ charter.sol
@@ -0,0 +1,24 @@
+pragma solidity 0.5.8;
+
+contract    ContratoMSC {
+    
+    
+    struct Carga {
+        uint256 peso;
+        string conteudo;
+        string proprietario;
+        
+        
+        
+    }
+    
+    struct Afretador {
+        
+        address payable Afretador;
+        
+        
+        
+    }
+    
+    
+}
