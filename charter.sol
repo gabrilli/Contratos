75906327097f70e777b89393759bbaede6183419
gabrilli <50384585+gabrilli@users.noreply.github.com>
2019-05-21 16:45:45 -0300

    Update charter.sol

--- charter.sol
+++ charter.sol
@@ -6,9 +6,17 @@ contract    ContratoBroker {
     string _NomeBroker;
     address payable _Armador;
     address payable _Onwer;
+    uint256 _Frete;
+    uint256 _CapacidadeVolume;
+    uint256 _CapacidadePeso;
+    bool CargaAutorizada;
+    uint256 _volume;
+    uint256 _peso;
+    
+    bool CapacidadeEsgotada;
     
     
-    struct Carga {
+    struct CargaA {
         uint256 peso;
         string conteudo;
         uint256 volume;
@@ -16,21 +24,84 @@ contract    ContratoBroker {
         
     }
     
+    struct CargaB {
+        uint256 peso;
+    }
     
-    Carga[] Catalogo;
-    
+    struct CargaC {
+        uint256 volume;
+        
+    }
+   
     constructor (
         
         address payable CarteiraBroker,
-        string memory NomeBroker
-        )
+        string memory NomeBroker,
+        address payable Armador,
+        uint256 Frete,
+        uint256 CapacidadePeso,
+        uint256 CapacidadeVolume
+        
+    )
     
     public {
      
      _CarteiraBroker = CarteiraBroker;
      _NomeBroker = NomeBroker;
-     
-     
+     _Armador = Armador;
+     _Frete = Frete;
+     _CapacidadePeso = CapacidadePeso;
+     _CapacidadeVolume = CapacidadeVolume;
      
      
     }
+    
+    CargaA[] ListaA;
+    CargaB[] ListaB;
+    CargaC[] ListaC;
+   
+       
+       
+       
+       event VolumeAdicionado (uint256 peso, uint256 volume);
+       
+   
+     
+     
+    
+    
+    function ConhecimentoDeCarga (uint256 peso, string memory conteudo, uint256 volume, address proprietario) public {
+       
+      
+       
+       CargaA memory CargaRegistradaA = CargaA (peso, conteudo, volume, proprietario);
+       CargaB memory CargaRegistradaB = CargaB (peso);
+       CargaC memory CargaRegistradaC = CargaC (volume);
+     
+      ListaA.push (CargaRegistradaA);
+      ListaB.push (CargaRegistradaB);
+      ListaC.push (CargaRegistradaC);
+      
+      
+      
+      
+       
+       emit VolumeAdicionado (peso, volume);
+       
+       CargaAutorizada = true;
+       
+    }  
+    
+     
+    function CalculoFrete (CargaB memory, CargaC memory) public view returns (uint256 Preco) {
+        
+        require (CargaAutorizada == true, "RegistreaCarga");
+        
+        Preco = peso*volume*_Frete;
+        return Preco;
+        
+        
+    }
+    
+     
+}
