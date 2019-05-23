d6de0f0e3018e03966fe0c9c7a0608b0367571bc
gabrilli <50384585+gabrilli@users.noreply.github.com>
2019-05-23 16:00:53 -0300

    Create DigitalID.sol

---
+++ DigitalID.sol
@@ -0,0 +1,45 @@
+pragma solidity 0.5.8;
+
+contract DigitalID {
+    
+    address payable Onwer;
+    uint256 Valor;
+    //bool InformcaoAnteriormenteRegistrada;
+    
+       struct DigitalID {
+       address Cliente;
+       string Nome;
+       uint256 CPF;
+       uint256 Celular;
+       uint256 RG;
+       
+       
+   }
+    DigitalID[] ListaClientesA;
+    mapping (address => DigitalID) public ListaClientesM;
+    
+    
+    event NovaID (address Cliente, uint Identificador);
+    
+    constructor (address payable _Onwer, uint _Valor) 
+        public {
+            Onwer = _Onwer;
+            Valor = _Valor;
+        }
+    
+    
+    function Cadastro (address Cliente, string memory Nome, uint256 CPF, uint256 Celular, uint256 RG, string memory Senha) public payable returns (uint) {
+        
+        require (msg.value == Valor, "O preco e 100 wei");
+        
+        DigitalID memory Temp = DigitalID (Cliente, Nome, CPF, Celular, RG);
+        ListaClientesA.push(Temp);
+        ListaClientesM[Cliente] = Temp;
+    }    
+        
+    
+        
+       
+      
+    
+}
